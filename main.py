c8d7e816c07c6b7aef843e7bc6505d31b9e285fa
abdulhai1234 <abdulhaik010@gmail.com>
2023-07-23 13:34:09 +0500

    done

--- main.py
+++ main.py
@@ -38,10 +38,26 @@ output_language_code = languages[output_language]
 
 uploaded_file = st.file_uploader("Choose an audio file", type=["wav","mp3"])
 
+if uploaded_file is None:
+    st.warning("Please upload a file")
 # Check if the selected language has changed
 # if st.session_state.get("prev_input_language") != input_language or st.session_state.get("prev_output_language") != output_language:
 #     # Reset the uploaded file
 #     uploaded_file = None
+
+# Check if the selected input language has changed
+if st.session_state.get("prev_input_language") != input_language:
+    # Reset the uploaded file
+    uploaded_file = None
+    # st.experimental_rerun()
+
+
+
+# Store the current selected input language in session state
+st.session_state["prev_input_language"] = input_language
+
+print(st.session_state.get("prev_input_language"))
+
 if os.path.exists("audio_to_save.wav"):
     os.remove('audio_to_save.wav')
 
@@ -51,26 +67,10 @@ if uploaded_file is not None:
     st.audio(uploaded_file, format='audio/wav')
 
 
-def preprocess_audio(data):
-    time.sleep(20)
-    rate, data =wavfile.read(data)
-    return rate,data
+
 
 
 rev_ai = RevAI(token=token,input_lang=input_language_code,output_lang=output_language_code)
-# result = rev_ai.process_audio(uploaded_file.getvalue())
-# print(uploaded_file.getvalue())
-# print(uploaded_file)
-
-
-# if uploaded_file is not None:
-    # Save the uploaded file to disk
-    # with open("uploaded_file.wav", "wb") as f:
-    #     f.write(uploaded_file.getvalue())
-
-    # Pass the file path to the submit_job_local_file method
-    # job = client.submit_job_local_file("uploaded_file.wav")
-    # result= rev_ai.process_audio("uploaded_file.wav")
 
 import os
 
@@ -78,19 +78,12 @@ import os
 if uploaded_file is not None:
     with st.spinner("Audio is translation please wait..."):
         temp_name="audio_to_save.wav"
-        # rate, data = wavfile.read(uploaded_file)
         with open(temp_name, "wb") as f:
             f.write(uploaded_file.getvalue())
-        # rate,data= rev_ai.process_audio(uploaded_file)
         location= rev_ai.process_audio(temp_name)
-        os.remove(temp_name)
-        # Save preprocessed data to a new wav file
-        # wavfile.write('preprocessed.wav', rate, preprocessed_data.astype(np.int16))
-
-        # wavfile.write('preprocessed.wav', rate, data.astype(np.int16))
-    
-    # Display the preprocessed audio file in a playlist
+        os.remove(temp_name)    
     st.header("Translate file")
+    uploaded_file=None
     st.audio(location, format='audio/wav')
 
 
